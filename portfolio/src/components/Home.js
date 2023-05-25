3f55d877dc727ef2ab9554e02a3ffc83ca33fa77
BaoWoW <baonguyen9392@gmail.com>
2023-05-25 14:27:12 -0700

    Update Home Page & Add Firebase Hosting

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,10 +1,23 @@
 import "../styles/Home.css";
+import react from '../assets/logos/react.png'
+import js from '../assets/logos/js.png'
+import jest from '../assets/logos/jest.png'
+import git from '../assets/logos/git.png'
+import router from '../assets/logos/router.png'
+import github from '../assets/logos/github.png'
+import webpack from '../assets/logos/webpack.png'
+import npm from '../assets/logos/npm.png'
+import tailwind from '../assets/logos/tailwind.png'
+import avatar from '../assets/avatar.png'
 
 const Home = () => {
   return (
-    <div className="Home">
+      <div className="Home">
       <div className="homeMain">
-        <div className="homeTitle">Welcome!</div>
+        <div className="homeTitle">
+            <div><img src={avatar}/></div>
+            <h1>Welcome!</h1>
+        </div>
         <div className="introWrapper">
           <div className="introPara">
             I'm Bao Nguyen, an aspiring software developer from Sacramento,
@@ -20,7 +33,20 @@ const Home = () => {
           <div className="line"></div>
         </div>
       </div>
-      <div className="homeContent"></div>
+      <div className="homeContent">
+        <div>Home Content Here</div>
+        <ul className="skillSet">
+            <li><img src={react}/></li>
+            <li><img src={js}/></li>
+            <li><img src={router}/></li>
+            <li><img src={jest}/></li>
+            <li><img src={git}/></li>
+            <li><img src={github}/></li>
+            <li><img src={webpack}/></li>
+            <li><img src={npm}/></li>
+            <li><img src={tailwind}/></li>
+        </ul>
+      </div>
     </div>
   );
 };
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -3,16 +3,25 @@ import "../styles/Navbar.css";
 const Navbar = () => {
   const navigate = useNavigate();
 
+  window.onscroll = () => {
+    let winScroll = document.body.scrollTop || document.documentElement.scrollTop
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+    document.querySelector('.progress').style.width = `${scrolled}%`
+  }
   const navBtnEvent = (e) => {
-      const btn = e.target;
-      const active = document.querySelectorAll('.active')
-      active.forEach((el) => el.className = '')
-      btn.className = "active"
-      navigate(`/${(e.target.textContent).toLowerCase()}`);
+    const btn = e.target;
+    const active = document.querySelectorAll(".active");
+    active.forEach((el) => (el.className = ""));
+    btn.className = "active";
+    document.querySelector('.progress').style.width = '0%'
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    navigate(`/${e.target.textContent.toLowerCase()}`);
   };
   return (
     <div className="Navbar">
-      <div>Picture/Something Here</div>
+      <div>Picture Here</div>
       <div className="navbarBtns">
         <button type="button" onClick={navBtnEvent}>
           <div>Home</div>
@@ -26,6 +35,9 @@ const Navbar = () => {
         <button type="button" onClick={navBtnEvent}>
           <div>Resume</div>
         </button>
+      </div>
+      <div className="progressWrapper">
+        <div className="progress"></div>
       </div>
     </div>
   );
