31ce86e69f0062fa0010baa950e8a6d8904adc99
BaoWoW <baonguyen9392@gmail.com>
2023-05-30 17:13:10 -0700

    Update Home Projects Info & Styles

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -44,13 +44,17 @@ const Home = (props) => {
           <h1>About Me</h1>
           <div>
             <p>
-              Hello! I'm a health science graduate who has transitioned into software development.
-              I believe software development is one of the most impactful ways to improve people's lives.
-              My health science background has given me a different view on designing apps for the user, 
-              which has helped me understand the need to make technology better and more accessible.
-              My gritty nature has taught me to approach learning with determination and has allowed me to successfully transition from a health science background to software development.
-              I'm excited to apply my skills and knowledge to software development.
-              I hope to grow and continue learning in this amazing field!
+              Hello! I'm a health science graduate who has transitioned into
+              software development. I believe software development is one of the
+              most impactful ways to improve people's lives. My health science
+              background has given me a different view on designing apps for the
+              user, which has helped me understand the need to make technology
+              better and more accessible. My gritty nature has taught me to
+              approach learning with determination and has allowed me to
+              successfully transition from a health science background to
+              software development. I'm excited to apply my skills and knowledge
+              to software development. I hope to grow and continue learning in
+              this amazing field!
             </p>
             <ul className="skillSet">
               <li>
@@ -106,7 +110,7 @@ const Home = (props) => {
         </div>
         <ul className="projects">
           <h1>Projects</h1>
-          <li>
+          <li className="project">
             <div>
               <a href="https://instagramclone-f3e9c.web.app/">
                 <h3>Itstagram</h3>
@@ -117,37 +121,45 @@ const Home = (props) => {
                 well as comment on other posts. Users can create a new profile,
                 login with Google, or sign in with a test account.
               </p>
-              <div>
-                Developed using React, test driven development (TDD), and a BaaS
-                (Firebase) to store data.
-              </div>
+              <ul>
+                <li>React</li>
+                <li>JavaScript, HTML, & CSS</li>
+                <li>Google Firebase (NoSQL database)</li>
+                <li>Third party libraries integration</li>
+                <li>Test driven development (TDD)</li>
+              </ul>
             </div>
             <div className="imgContainer">
               <img src={imgArray[0]} />
               <img src={imgArray[1]} />
             </div>
           </li>
-          <li>
+          <li className="project">
             <div className="imgContainer">
               <img src={imgArray[2]} />
               <img src={imgArray[3]} />
             </div>
             <div>
               <a href="https://shop-bc80d.web.app/">
-                <b>Shop</b>
+                <h3>Shop</h3>
               </a>
               <p>
-                A modern climbing shop for users to purchase a variety of
+                A modern climbing e-commerce platform for users to purchase a variety of
                 products. The shop features a cart for users to check out their
-                personalized items.
+                personalized items as well as item catergories.
               </p>
-              <div>App developed using React Router, React, & TDD.</div>
+              <ul>
+                <li>TDD</li>
+                <li>React</li>
+                <li>React Router</li>
+                <li>JavaScript, HTML, & CSS</li>
+              </ul>
             </div>
           </li>
-          <li>
+          <li className="project">
             <div>
               <a href="https://knight-travails.web.app/">
-                <b>Knight Travails</b>
+                <h3>Knight Travails</h3>
               </a>
               <p>
                 An app that shows the shortest amount of moves it takes for a
@@ -155,24 +167,28 @@ const Home = (props) => {
                 amount of moves is indicated by a color transition rather than a
                 numerical value.
               </p>
-              <div>
-                App developed using React and knowledge of data structures &
-                search algorithms.
-              </div>
+              <ul>
+                <li>React</li>
+                <li>JavaScript, HTML, & CSS</li>
+                <li>Object-oriented programming (OOP)</li>
+                <li>
+                  Graph data structure & breadth-first search (BFS) algorithm
+                </li>
+              </ul>
             </div>
             <div className="imgContainer">
               <img src={imgArray[4]} />
               <img src={imgArray[5]} />
             </div>
           </li>
-          <li>
+          <li className="project">
             <div className="imgContainer">
               <img src={imgArray[6]} />
               <img src={imgArray[7]} />
             </div>
             <div>
               <a href="https://whereswaldo-29041.web.app/">
-                <b>Where's Waldo</b>
+                <h3>Where's Waldo</h3>
               </a>
               <p>
                 A photo-tagging app based off of "Where's Waldo" game where the
@@ -180,10 +196,11 @@ const Home = (props) => {
                 three characters, user has option to publish their time and view
                 the scoreboard.
               </p>
-              <div>
-                App developed using frontend skills with Firebase as a backend
-                to store scoreboard.
-              </div>
+              <ul>
+                <li>React</li>
+                <li>Google Firebase</li>
+                <li>JavaScript, HTML, & CSS</li>
+              </ul>
             </div>
           </li>
           <button onClick={() => props.reroute("", "projects")}>
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import "../styles/Navbar.css";
 const Navbar = (props) => {
 
